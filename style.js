68ce9f36861ed8aca5191a0620ffcefb4cc485d5
Ilgiz Saburov <barysaburov@gmail.com>
2024-07-23 17:09:05 +0300

    bug fix

--- style.js
+++ style.js
@@ -1,4 +1,4 @@
-const userButter = +prompt(' Установите время готовки в секундах');
+const userButter = +prompt('Установите время готовки в секундах');
     switch (true) {
         case (userButter <= 20):
             alert('20 - Баллов вкусности');

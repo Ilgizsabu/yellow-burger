bc30a4655c64ec422973e8073d3ea7d0dd79f711
Ilgiz Saburov <barysaburov@gmail.com>
2024-07-23 22:42:49 +0300

    bug fix

--- style.js
+++ style.js
@@ -1,18 +1,19 @@
 const userButter = +prompt('Установите время готовки в секундах');
-const grade = 'Баллов вкусности!';
-    switch (true) {
-        case (userButter <= 20):
-            alert(`20: ${grade}`);
-            break;
-        case (userButter <= 40):
-            alert(`40: ${grade}`);
-            break;
-        case (userButter <= 144):
-            alert(`80: ${grade}`);
-            break;
-        case (userButter <= 200):
-            alert(`100: ${grade}`);
-            break;
-        case (userButter >= 201):
-            alert('0 - Баллов вкусности!, бургер сгорел((');
-    }
+const grade = 'Колличество баллов вкусности - ';
+const maxTaste = Math.round(userButter * 0.01) * 100;
+const butTaste = 'Бургер сгорел((';
+const data = (userButter >= 0);
+
+switch (data) {
+    case (userButter <= 39):
+        alert(grade + ' ' + userButter);
+        break;
+    case (userButter <= 99):
+        alert(grade + ' ' + userButter);
+        break;
+    case (userButter <= 120):    
+        alert(grade + ' ' + maxTaste);
+        break;
+    default:
+        alert(butTaste);
+}

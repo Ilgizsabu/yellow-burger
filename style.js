4f27234112e28b95333bcfd7a6981a5d4f23c747
Ilgiz Saburov <barysaburov@gmail.com>
2024-07-25 23:13:44 +0300

    add a new solution

--- style.js
+++ style.js
@@ -2,18 +2,43 @@ const userButter = +prompt('Установите время готовки в с
 const grade = 'Колличество баллов вкусности - ';
 const maxTaste = Math.round(userButter * 0.01) * 100;
 const butTaste = 'Бургер сгорел((';
-const data = (userButter >= 0);
 
-switch (data) {
-    case (userButter <= 39):
-        alert(grade + ' ' + userButter);
-        break;
-    case (userButter <= 99):
-        alert(grade + ' ' + userButter);
-        break;
-    case (userButter <= 120):    
-        alert(grade + ' ' + maxTaste);
-        break;
-    default:
-        alert(butTaste);
+if (userButter <= 39) {
+    alert(grade + ' ' + userButter);
 }
+
+if (userButter >= 40 && userButter <= 99) {
+    alert(grade + ' ' + userButter);
+}
+
+if (userButter >= 100 && userButter <= 120) {    
+    alert(grade + ' ' + maxTaste);
+}
+
+if (userButter > 120) {    
+    alert(butTaste);    
+}
+
+
+
+//That is another solution
+
+// const userButter = +prompt('Установите время готовки в секундах');
+// const grade = 'Колличество баллов вкусности - ';
+// const maxTaste = Math.round(userButter * 0.01) * 100;
+// const butTaste = 'Бургер сгорел((';
+// const data = (userButter >= 0);
+
+// switch (data) {
+//     case (userButter <= 39):
+//         alert(grade + ' ' + userButter);
+//         break;
+//     case (userButter <= 99):
+//         alert(grade + ' ' + userButter);
+//         break;
+//     case (userButter <= 120):    
+//         alert(grade + ' ' + maxTaste);
+//         break;
+//     default:
+//         alert(butTaste);
+// }

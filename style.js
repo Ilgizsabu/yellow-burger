656a98108bec2f0968d908921c71389376a8809b
Ilgiz Saburov <barysaburov@gmail.com>
2024-08-01 15:00:08 +0300

    bags to fix

--- style.js
+++ style.js
@@ -12,27 +12,3 @@ if (userButter > 99 && userButter < 120) {
 } else if (userButter > 119) {    
     alert(badTaste);    
 }
-
-
-
-//That is first solution
-
-// const userButter = +prompt('Установите время готовки в секундах');
-// const grade = 'Колличество баллов вкусности - ';
-// const maxTaste = Math.round(userButter * 0.01) * 100;
-// const badTaste = 'Бургер сгорел((';
-// const data = (userButter >= 0);
-
-// switch (data) {
-//     case (userButter <= 39):
-//         alert(grade + ' ' + userButter);
-//         break;
-//     case (userButter <= 99):
-//         alert(grade + ' ' + userButter);
-//         break;
-//     case (userButter <= 120):    
-//         alert(grade + ' ' + maxTaste);
-//         break;
-//     default:
-//         alert(badTaste);
-// }
